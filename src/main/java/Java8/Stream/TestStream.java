5db1229e58d62ee38a0944ebc1214fadd70c6a13
Vaibhav <vaibhav.katkar2302@gmail.com>
2025-05-26 17:31:32 +0530

    Added for stream practise

--- src/main/java/Java8/Stream/TestStream.java
+++ src/main/java/Java8/Stream/TestStream.java
@@ -1,28 +1,69 @@
 package Java8.Stream;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.*;
 
 public class TestStream {
 
     public static void main(String[] args) {
-        List<Integer> integerList = Arrays.asList(3, 5, 45, 84, 3, 7, 9, 4, 2, 54, 0, 1, 0, 1);
-        List<Integer> resultList = integerList.stream().map(x -> x * x).toList();
-        System.out.println(resultList);
 
-        List<Integer> filteredList = integerList.stream().filter(x -> x % 2 == 0).toList();
-        System.out.println(filteredList);
+        practiceCode();
+        //challenge 1
+        List<Employee> employees = Arrays.asList(
+                new Employee("Alice", 30, 60000),
+                new Employee("Bob", 24, 45000),
+                new Employee("Charlie", 28, 52000),
+                new Employee("David", 35, 48000),
+                new Employee("Eve", 40, 75000)
+        );
 
-        List<Integer> mapList = filteredList.stream().map(x -> x / 2).toList();
-        System.out.println(mapList);
+        //challenge 1
+        List<Employee> empList = employees.stream().filter(x -> x.getSalary() > 50000).collect(Collectors.toList());
+        System.out.println(empList);
 
-        List<Integer> list = integerList.stream().filter(x -> x % 2 == 0)
-                .map(x -> x / 2)
-                .distinct()
-                .sorted((a, b) -> b - a)
-                .toList();
+        //challenge 2
+        List<String> empList2 = employees.stream()
+                .filter(x -> x.getAge() < 35)
+                .map(x->x.getName())
+                .collect(Collectors.toList());
+        System.out.println(empList2);
 
-        System.out.println(list);
+        //challenge 3
+        Optional<Employee> employee = employees.stream().max(Comparator.comparingDouble(x->x.getSalary()));
+        System.out.println(employee.get());
+
+        //challenge 4
+        Map<Integer, List<Employee>> collect = employees.stream().collect(Collectors.groupingBy(x -> x.getAge()));
+        System.out.println(collect);
+
+        //challenge 5
+        Double collect1 = employees.stream().collect(Collectors.averagingDouble(x -> x.getSalary()));
+        System.out.println(collect1);
+
+        // challenge 6
+        Map<String, Double> collect2 = employees
+                .stream()
+                .collect(Collectors.toMap(x -> x.getName(),x -> x.getSalary(),(x,y)->x + y));
+        System.out.println(collect2);
+
+    }
+
+    private static void practiceCode() {
+        // stream of list
+        List<String> list = Arrays.asList("a","c","d","i");
+        Stream<String> stream = list.stream();
+        // stream of array
+        String[] array = {"abc","xyz","pkw"};
+        Stream<String> stream1 = Arrays.stream(array);
+        // Stream by stream
+        Stream<Integer> integerStream = Stream.of(1, 4, 6, 8, 8);
+        // infinite stream or with limit
+        List<Integer> list1 = Stream.generate(() -> 100).limit(100).toList();
+
+        // Parallel Stream works with cpu intensive task, large data set
+        // parallel stream cannot be used if stream values are interdependent
+        List<Integer> list2 = Stream.iterate(1, x -> x + 1).limit(200000).toList();
+        List<Integer> list3 = list2.parallelStream().map(x -> x * 2).toList();
 
     }
 }
